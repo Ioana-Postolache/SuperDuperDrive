a35d6f9d39edc06e3cb4b0999cbc9f524db776fc
postolac <postolac@adobe.com>
2021-04-04 20:00:19 +0300

    Added project folders and model classes

---
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/model/Files.java
@@ -0,0 +1,69 @@
+package com.udacity.jwdnd.course1.cloudstorage.model;
+
+import java.sql.Blob;
+
+public class Files {
+    private int fileId;
+    private String fileName;
+    private String contentType;
+    private String fileSize;
+    private int userId;
+    private Blob fileData;
+
+    public Files(int fileId, String fileName, String contentType, String fileSize, int userId, Blob fileData) {
+        this.fileId = fileId;
+        this.fileName = fileName;
+        this.contentType = contentType;
+        this.fileSize = fileSize;
+        this.userId = userId;
+        this.fileData = fileData;
+    }
+
+    public int getFileId() {
+        return fileId;
+    }
+
+    public void setFileId(int fileId) {
+        this.fileId = fileId;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getContentType() {
+        return contentType;
+    }
+
+    public void setContentType(String contentType) {
+        this.contentType = contentType;
+    }
+
+    public String getFileSize() {
+        return fileSize;
+    }
+
+    public void setFileSize(String fileSize) {
+        this.fileSize = fileSize;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public Blob getFileData() {
+        return fileData;
+    }
+
+    public void setFileData(Blob fileData) {
+        this.fileData = fileData;
+    }
+}
